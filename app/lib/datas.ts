c4dffae304a68a8e4777260c4ba9d863a9cc8103
Ayomide <ayomideolakunlea1@gmail.com>
2026-08-02 17:17:17 +0100

    Added some more images for gallery

--- app/lib/datas.ts
+++ app/lib/datas.ts
@@ -10,6 +10,12 @@ import G1 from "@/public/assets/images/grads/grad1.jpeg"
 import G2 from "@/public/assets/images/grads/grad2.jpeg"
 import G3 from "@/public/assets/images/grads/grad3.jpeg"
 import G4 from "@/public/assets/images/grads/grad4.jpeg"
+import E9 from "@/public/assets/images/events/eve1.jpeg"
+import E10 from "@/public/assets/images/events/eve2.jpeg"
+import A1 from "@/public/assets/images/academics/aca1.jpeg"
+import A2 from "@/public/assets/images/academics/aca2.jpeg"
+import A3 from "@/public/assets/images/academics/aca4.jpeg"
+import S1 from "@/public/assets/images/sports/sport.jpeg"
 
 
 export const GALLERY_ITEMS = [
@@ -27,6 +33,16 @@ export const GALLERY_ITEMS = [
         img: E3,
         cat: "events",
         h: "48",
+    },
+    {
+        img: E9,
+        cat: "events",
+        h: "h-40",
+    },
+    {
+        img: E10,
+        cat: "events",
+        h: "h-58",
     },
     {
         img: E4,
@@ -72,5 +88,25 @@ export const GALLERY_ITEMS = [
         img: G4,
         cat: "graduation",
         h: "h-38",
+    },
+    {
+        img: A1,
+        cat: "academics",
+        h: "h-88",
+    },
+    {
+        img: A2,
+        cat: "academics",
+        h: "h-35",
+    },
+    {
+        img: A3,
+        cat: "academics",
+        h: "h-46",
+    },
+    {
+        img: S1,
+        cat: "sports",
+        h: "h-54",
     },
 ]
